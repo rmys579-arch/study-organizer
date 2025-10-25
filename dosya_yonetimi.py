b6f0b15f1f23389b006942aac2fc7abed9fce8e9
Ceyda111 <kirmizibayrakceyda@gmail.com>
2025-10-25 12:19:35 +0300

    Update dosya_yonetimi.py

---
+++ dosya_yonetimi.py
@@ -0,0 +1,12 @@
+
+import os # dosyayı tekrar silmemiz  yeniden adlandıramız veya sınıflandırmamız için 
+import shutil # dosyaları taşımak için 
+# sınıflandırma (sözlük) partı
+Sınıflandırma = {   
+    "Belge": [ ".pdf" , ".docx" , ".txt" , ]  ,   
+    "Görsel" : [ ".jpg", ".jpeg", ".gif"   ],
+    "Sunum" : [ ".pptx" , ".ppt", ".key",  ]
+
+}
+
+
